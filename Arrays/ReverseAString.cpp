ca3a52d909e2dff6e730cfa44baffc106496e16f
nishantgta <2020csb1103@iitrpr.ac.in>
2026-01-11 14:52:02 +0530

    Reverse a string

---
+++ Arrays/ReverseAString.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+void reverseString(vector<char>&s){
+    int n=s.size();
+    for(int i=0;i<n/2;i++){
+        char ch=s[i];
+        s[i]=s[n-i-1];
+        s[n-i-1]=ch;
+    }
+}
+int main(){
+    vector<char>s={'h','e','l','l','o'};
+    vector<char>s1={'H','a','n','n','a','h'};
+    reverseString(s);
+    reverseString(s1);
+    for(auto x:s){
+        cout << x << " ";
+    }
+    cout<<endl;
+    for(auto x:s1) cout << x << " ";
+    return 0;
+}
